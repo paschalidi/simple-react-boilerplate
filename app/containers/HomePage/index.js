36f66a1df5c5b1ee30c12b28dd15804770a89b4c
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-04-09 13:52:40 +0300

    BOILERPLATE CLEAN.Creates component for example purposes.

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,12 +11,14 @@
 
 import React from 'react';
 
+import Header from '../../components/Header'
+
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        Hello Marino
-      </h1>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
