d29b7ebdf3a68b654e476f8e542af6e81fecd31c
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-05-01 20:15:11 +0300

    removes saga

--- app/store.js
+++ app/store.js
@@ -5,17 +5,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { fromJS } from 'immutable';
 import { routerMiddleware } from 'react-router-redux';
-import createSagaMiddleware from 'redux-saga';
 import createReducer from './reducers';
-
-const sagaMiddleware = createSagaMiddleware();
 
 export default function configureStore(initialState = {}, history) {
   // Create the store with two middlewares
   // 1. sagaMiddleware: Makes redux-sagas work
   // 2. routerMiddleware: Syncs the location/URL path to the state
   const middlewares = [
-    sagaMiddleware,
     routerMiddleware(history),
   ];
 
